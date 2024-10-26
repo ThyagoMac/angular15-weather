df623210a3d3d67521c1d4b520f1abfc373929cc
thyagomac <thyago.cormick@gmail.com>
2024-10-26 17:29:05 -0300

    weather.interfece.ts created and implemented in weather-home.component.ts;

--- src/app/modules/weather/page/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
+import { WeatherDatasInterface } from 'src/app/models/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent {}
+export class WeatherHomeComponent implements OnInit {
+  city = 'São Paulo';
+  weatherDatas!: WeatherDatasInterface;
+
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.getWeatherDatas(this.city);
+  }
+
+  getWeatherDatas(city: string): void {
+    this.weatherService.getWeatherDatas(city).subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+}
